50f88bb0ff983a572e3b7713eb4f9e59fc40362e
Tim Seckinger <seckinger.tim@gmail.com>
2017-07-29 21:02:52 +0200

    test(e2e): element check settings

---
+++ e2e/src/check-elements/e2e-check-elements.components.spec.ts
@@ -0,0 +1,119 @@
+import * as chai from 'chai';
+import 'mocha';
+import { $, browser } from 'protractor';
+import { displayedPersons } from '../data/displayed-persons';
+import { emptyPerson, persons } from '../data/persons';
+
+chai.should();
+
+describe('The checkElements setting', () => {
+  describe('"None"', () => {
+    beforeEach(async () => await browser.get('/check-no-elements'));
+
+    it('should render the empty item included in the array', async () => {
+      (await displayedPersons()).should.eql([...persons, emptyPerson]);
+    });
+
+    it('should still render an item that has been emptied', async () => {
+      await $('#empty-first').click();
+
+      (await displayedPersons()).should.eql([
+        emptyPerson,
+        ...persons.slice(1),
+        emptyPerson,
+      ]);
+    });
+
+    it('should still render an item that has been replaced with an empty item', async () => {
+      await $('#replace-first-empty').click();
+
+      (await displayedPersons()).should.eql([
+        emptyPerson,
+        ...persons.slice(1),
+        emptyPerson,
+      ]);
+    });
+
+    it('should render an empty item that has been prepended', async () => {
+      await $('#prepend-empty').click();
+
+      (await displayedPersons()).should.eql([
+        emptyPerson,
+        ...persons,
+        emptyPerson,
+      ]);
+    });
+  });
+
+  describe('"New"', () => {
+    beforeEach(async () => await browser.get('/check-new-elements'));
+
+    it('should not render the empty item included in the array', async () => {
+      (await displayedPersons()).should.eql([
+        persons[0],
+        persons[2],
+        emptyPerson,
+      ]);
+    });
+
+    it('should still render an item that has been emptied', async () => {
+      await $('#empty-first').click();
+
+      (await displayedPersons()).should.eql([
+        emptyPerson,
+        persons[2],
+        emptyPerson,
+      ]);
+    });
+
+    it('should no longer render an item that has been replaced with an empty item', async () => {
+      await $('#replace-first-empty').click();
+
+      (await displayedPersons()).should.eql([persons[2], emptyPerson]);
+    });
+
+    it('should not render an empty item that has been prepended', async () => {
+      await $('#prepend-empty').click();
+
+      (await displayedPersons()).should.eql([
+        persons[0],
+        persons[2],
+        emptyPerson,
+      ]);
+    });
+  });
+
+  describe('"All"', () => {
+    beforeEach(async () => await browser.get('/check-all-elements'));
+
+    it('should not render the empty item included in the array', async () => {
+      (await displayedPersons()).should.eql([
+        persons[0],
+        persons[2],
+        emptyPerson,
+      ]);
+    });
+
+    it('should no longer render an item that has been emptied', async () => {
+      await $('#empty-first').click();
+
+      (await displayedPersons()).should.eql([persons[2], emptyPerson]);
+    });
+
+    it('should no longer render an item that has been replaced with an empty item', async () => {
+      await $('#replace-first-empty').click();
+
+      (await displayedPersons()).should.eql([persons[2], emptyPerson]);
+    });
+
+    it('should not render an empty item that has been prepended', async () => {
+      await $('#prepend-empty').click();
+
+      (await displayedPersons()).should.eql([
+        persons[0],
+        persons[2],
+        emptyPerson,
+      ]);
+    });
+  });
+});
