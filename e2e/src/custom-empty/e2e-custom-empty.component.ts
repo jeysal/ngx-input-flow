8e18181b5cc6f70a0af27877a3846492116860db
Tim Seckinger <seckinger.tim@gmail.com>
2017-07-30 17:02:37 +0200

    test(e2e): checkElements takes emptyWhen into account

--- e2e/src/custom-empty/e2e-custom-empty.component.ts
+++ e2e/src/custom-empty/e2e-custom-empty.component.ts
@@ -8,13 +8,18 @@ export interface Item {
 @Component({
   selector: 'ngx-input-flow-e2e-custom-empty',
   template: `
-        <div [(ngxInputFlow)]="items" [emptyItem]="emptyItem" [emptyWhen]="emptyWhen" #input="ngxInputFlow">
+        <div [(ngxInputFlow)]="items"
+             checkElements="All"
+             [emptyItem]="emptyItem"
+             [emptyWhen]="emptyWhen"
+             #input="ngxInputFlow">
           <div *ngFor="let item of input.flow()"
                  [ngxInputFlowModel]="item">
             <input type="text" [(ngModel)]="item.product" name="product">
             <input type="number" [(ngModel)]="item.count" name="count">
           </div>
         </div>
+        <button id="empty-first" (click)="emptyFirst()">Empty first item</button>
     `,
 })
 export class E2eCustomEmptyComponent {
@@ -22,4 +27,11 @@ export class E2eCustomEmptyComponent {
   emptyItem = () => ({ product: '', count: 1 });
   emptyWhen = (item: Item) =>
     !item.product && (!item.count || item.count === 1); // tslint:disable-line:semicolon
+
+  emptyFirst() {
+    const empty = this.emptyItem();
+
+    this.items[0].product = empty.product;
+    this.items[0].count = empty.count;
+  }
 }
