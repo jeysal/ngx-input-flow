6bb9c75b255aad99573c0ebae5bf50da64a71cf0
Tim Seckinger <seckinger.tim@gmail.com>
2017-12-25 12:58:49 +0100

    build(rollup): update config to new format

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,17 @@
 export default {
   input: 'dist/index.js',
-  name: 'inputflow',
-  sourcemap: true,
   output: {
+    name: 'inputflow',
     file: 'dist/bundles/inputflow.umd.js',
     format: 'umd',
-  },
-  globals: {
-    '@angular/core': 'ng.core',
-    'rxjs/Observable': 'Rx',
-    'rxjs/Subject': 'Rx',
-    'rxjs/observable/merge': 'Rx.Observable',
-    'rxjs/observable/timer': 'Rx.Observable',
-    'rxjs/operators': 'Rx.operators',
+    sourcemap: true,
+    globals: {
+      '@angular/core': 'ng.core',
+      'rxjs/Observable': 'Rx',
+      'rxjs/Subject': 'Rx',
+      'rxjs/observable/merge': 'Rx.Observable',
+      'rxjs/observable/timer': 'Rx.Observable',
+      'rxjs/operators': 'Rx.operators',
+    },
   },
 };
